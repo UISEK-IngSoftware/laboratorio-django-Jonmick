9e5da185a611fef71bac902917cd3f70cbaf54cf
Jonmick <jonathan.jm656@gmail.com>
2025-08-05 17:34:26 -0500

    Cambios Oauth2

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 router = DefaultRouter()
 router.register(r'pokemons', views.PokemonViewSet)
+router.register(r'trainers', views.TrainerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from pokedex.models import Pokemon
-from .serializers import PokemonSerializers
+from pokedex.models import Trainer
+from .serializers import PokemonSerializers, TrainerSerializers
 from oauth2_provider.contrib.rest_framework import OAuth2Authentication, TokenHasScope
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
@@ -15,4 +16,15 @@ class PokemonViewSet(viewsets.ModelViewSet):
         if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
             return [IsAuthenticated(), TokenHasScope()]
         return [AllowAny()]
+
+class TrainerViewSet(viewsets.ModelViewSet):
+    queryset = Trainer.objects.order_by('name')
+    serializer_class = TrainerSerializers
     
+    authentication_classes = [OAuth2Authentication]
+    required_scopes = ['write']
+    
+    def get_permissions(self):
+        if self.request.method in ['POST', 'PUT', 'PATCH', 'DELETE']:
+            return [IsAuthenticated(), TokenHasScope()]
+        return [AllowAny()]
